1ba541801dbfa33068ff353cf2aa77ba82f5c2c2
ntarakcija <ntarakcija1@etf.unsa.ba>
2018-03-30 17:39:43 +0200

    Prvi commit

---
+++ app/src/main/java/ba/unsa/etf/rma/nedim_tarakcija/rma18tarakija16925/KategorijeAkt.java
@@ -0,0 +1,13 @@
+package ba.unsa.etf.rma.nedim_tarakcija.rma18tarakija16925;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class KategorijeAkt extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_kategorije_akt);
+    }
+}
